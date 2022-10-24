1006fb59bf6db9260b22cbf017660bc61cb3b6a0
farukshin_n <pere--noel@yandex.ru>
2022-10-24 17:33:25 +0400

    fix: one unused import

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -28,7 +28,6 @@ import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
